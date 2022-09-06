591bfda948da9e682595dc6bc5491e61304e9630
vocvachcungthanh <vocvachcungthanh@gmail.com>
2022-09-06 08:08:32 +0700

    upload: header

--- src/layouts/components/Header/Header.vue
+++ src/layouts/components/Header/Header.vue
@@ -1,14 +1,44 @@
 <template>
   <header class="wrapper">
     <div class="inner">
-      <!-- Logo -->
+      <font-awesome-icon :icon="{ faUserSecret }" />
+
+      <div class="logo">
+        <img :src="iconLogo" alt="Tiktok" />
+      </div>
+      <div class="search">
+        <input
+          type="text"
+          placeholder="Search accounts and videos"
+          spellcheck="false"
+        />
+        <button class="clear">
+          <!-- Clear -->
+        </button>
+
+        <!-- Loading -->
+
+        <button class="search-btn">
+          <!-- Search -->
+        </button>
+      </div>
     </div>
   </header>
 </template>
 
 <script>
+import images from "@/assets/images";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+
 export default {
   name: "header-default",
+
+  data() {
+    return {
+      iconLogo: images.logo,
+      faUserSecret,
+    };
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import VueRouter from "vue-router";
 import App from "./App.vue";
 import { publicRoutes } from "./routes";
 
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* add font awesome icon component */
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 Vue.use(VueRouter);
 Vue.config.productionTip = false;
 
